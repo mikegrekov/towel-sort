ba36cb379d5d310cb53eff421880925a487719df
mikegrekov <svg200914@gmail.com>
2023-10-25 12:22:50 +0100

    autotest

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    if (!matrix) return [];
     return matrix.map((a, index) => {
-        return (index % 2 === 0) a: a.reverse();
-  })
+        if (index % 2 === 0) return a;
+        return a.reverse();
+  }).flat()
 }
